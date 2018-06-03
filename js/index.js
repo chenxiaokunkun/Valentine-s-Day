dceaaa25ba8263d541da56727580c24d485c38f6
chenxiaokunkun <1729803981@qq.com>
2018-06-03 17:05:23 +0800

    love

--- js/index.js
+++ js/index.js
@@ -1,10 +1,24 @@
 $(function(){
+    document.body,addEventListener('touchstar',function(){});//兼容
 
 
+    $('.btn>button').click(function(){
 
+        $(this).addClass('no').siblings().removeClasses('no');
 
+        if($(this).hasClass('wei')){
+            $(this).children('input').focus();
+        };
 
+    });
 
+    //活动规则
+    $('.huodonggz').click(function(){
+        $('.dic').fadeIn();//淡入
+    });
+    $('.yes').click(function(){
+        $('.dic').fadeOut();//淡出
+    })
 
 
 
